5c3b658e118fa557826f4cd4b9a576c293debfc8
Jupy Chen <jupychen@gmail.com>
2017-02-10 17:31:24 +0800

    test demo

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -2,7 +2,19 @@ require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
   ## 请补充一下测试代码
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get index" do
+    ## 创建10条数据
+    10.times do |i|
+      FactoryGirl.create(:article, title: "title-#{i}")
+    end
+
+    ## 访问 index
+    get '/articles.json'
+    ## 断言访问成功
+    assert_response :success
+    ## 断言接口返回的数据也有10条
+    return_json = JSON.parse(@response.body)
+    assert_equal return_json.length, 10
+    ##以下可以添加自己需要的断言
+  end
 end
---
+++ test/factories/articles.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :article do
+    title 'title test'
+    text 'text test'
+  end
+end
